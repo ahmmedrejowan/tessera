ff160ce5272c7cbe868e54218f226074d482bb79
K M Rejowan Ahmmed <kmrejowan@gmail.com>
2026-09-22 06:34:05 +0600

    Add command palette on Cmd/Ctrl+K

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,8 +1,5 @@
-import HomeOutlined from '@mui/icons-material/HomeOutlined';
-import SettingsOutlined from '@mui/icons-material/SettingsOutlined';
 import CircularProgress from '@mui/material/CircularProgress';
 import { useState } from 'react';
-import { EmptyState } from './components/EmptyState';
 import { ToastHost } from './components/Toast';
 import { AddMenu } from './import/AddMenu';
 import { DropOverlay } from './import/DropOverlay';
@@ -10,37 +7,41 @@ import { ImportDialog } from './import/ImportDialog';
 import { BrowsePage } from './pages/browse/BrowsePage';
 import { CollectionPage } from './pages/collections/CollectionPage';
 import { CollectionsPage } from './pages/collections/CollectionsPage';
+import { HomePage } from './pages/HomePage';
 import { InboxPage } from './pages/InboxPage';
+import { SettingsPage } from './pages/SettingsPage';
+import { CommandPalette } from './shell/CommandPalette';
 import { CopyConfirm } from './pages/projects/CopyConfirm';
 import { ProjectPage } from './pages/projects/ProjectPage';
 import { ProjectsPage } from './pages/projects/ProjectsPage';
 import { PackPage } from './pages/pack/PackPage';
-import { Page } from './pages/Placeholder';
 import { Welcome } from './pages/Welcome';
 import { AppShell } from './shell/AppShell';
 import { useLibraryState, useStats } from './state/library';
 import { useNav } from './state/nav';
 
-const PLACEHOLDERS = {
-  home: { title: 'Home', icon: HomeOutlined },
-  settings: { title: 'Settings', icon: SettingsOutlined },
-};
-
 function Current() {
   const route = useNav((s) => s.route);
-  if (route.to === 'browse') return <BrowsePage />;
-  if (route.to === 'pack') return <PackPage key={route.id} id={route.id} />;
-  if (route.to === 'inbox') return <InboxPage />;
-  if (route.to === 'collections') return <CollectionsPage />;
-  if (route.to === 'collection') return <CollectionPage key={route.id} id={route.id} />;
-  if (route.to === 'projects') return <ProjectsPage />;
-  if (route.to === 'project') return <ProjectPage key={route.id} id={route.id} />;
-  const p = PLACEHOLDERS[route.to];
-  return (
-    <Page title={p.title}>
-      <EmptyState icon={p.icon} title={`${p.title} is on its way`} body="This part of Tessera hasn't been built yet." />
-    </Page>
-  );
+  switch (route.to) {
+    case 'home':
+      return <HomePage />;
+    case 'browse':
+      return <BrowsePage />;
+    case 'pack':
+      return <PackPage key={route.id} id={route.id} />;
+    case 'inbox':
+      return <InboxPage />;
+    case 'collections':
+      return <CollectionsPage />;
+    case 'collection':
+      return <CollectionPage key={route.id} id={route.id} />;
+    case 'projects':
+      return <ProjectsPage />;
+    case 'project':
+      return <ProjectPage key={route.id} id={route.id} />;
+    case 'settings':
+      return <SettingsPage />;
+  }
 }
 
 export function App() {
@@ -72,6 +73,7 @@ export function App() {
       <AddMenu anchor={addAnchor} onClose={() => setAddAnchor(null)} />
       <ImportDialog />
       <CopyConfirm />
+      <CommandPalette />
       <DropOverlay enabled />
       <ToastHost />
     </>
